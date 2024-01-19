92ba41d06a1dacd556e196ae4347ca844d2d329a
nathalia <nathalia.souza@codeblox.com.br>
2024-01-18 22:10:14 -0300

    Adiciona controllers para get livro e list livro

---
+++ PosBooks/Controllers/GetLivroController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PosBooks.Services;
+using PosBooksCore.Models;
+
+namespace PosBooks.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class GetLivroController : ControllerBase
+    {
+        private readonly IBookService _bookService;
+
+        public GetLivroController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Book>> GetBook([FromRoute] int id)
+        {
+            var book = await _bookService.GetBook(id);
+
+            if (book == null) return NotFound();
+
+            return Ok(book);
+        }
+    }
+}
